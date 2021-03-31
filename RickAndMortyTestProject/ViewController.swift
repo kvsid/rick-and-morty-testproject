a8b4e63951993a220386a09d4ec5ba976b864e61
kvsid <kvsid.work@gmail.com>
2021-03-31 20:26:57 +0300

    Initial Commit

---
+++ RickAndMortyTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyTestProject
+//
+//  Created by Xenia Sidorova on 31.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
